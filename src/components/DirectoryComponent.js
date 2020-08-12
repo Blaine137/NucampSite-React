ff52dbf110c97149ba37fad0d9a003ce509f059f
Blaine Young <blaine.young3553@gmail.com>
2020-08-12 13:26:14 -0400

    added router parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,  CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     //object destructure props so the only props we get in this component is campsite and onClick
@@ -8,10 +9,12 @@ function RenderDirectoryItem({campsite, onClick}){
     return(
 
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name}/>
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name}/>
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
 
     );
