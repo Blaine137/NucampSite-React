88635dfda5e00bed5c3b691d3af409198ac5aec8
Blaine Young <blaine.young3553@gmail.com>
2020-08-11 21:37:58 -0400

    fixed the router error

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,7 @@ import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import Home from './HeaderComponent';
+import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
