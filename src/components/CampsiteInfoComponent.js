bc2e7cc13142f5ec1e940a96f2918e8f6baa936c
Blaine Young <blaine.young3553@gmail.com>
2020-08-22 11:39:13 -0400

    added comment form component which only contains the buttons so far

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 function RenderCampsite({campsite}){
 
@@ -21,61 +22,75 @@ function RenderCampsite({campsite}){
 
 }
 
-    function RenderComments({comments}) {
+function RenderComments({comments}) {
 
-        if(comments){
+    if(comments){
 
-            return(
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map( comment => { 
-                    
-                        return(
-                            <div key={comment.id}>
-                            <p>{comment.text}</p>
-                            <p>-- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                            </div>
-                        );
-                        
-                     })}
-                </div>
-            );
-
-        }else{
-            return <div> </div>;
-        }
-
-    }
-
-
-    function CampsiteInfo(props){
-
-            //if campsite is not null or undefined set to a row else a empty div
-        if(props.campsite){
-             return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <Breadcrumb>
-                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                            </Breadcrumb>
-                            <h2>{props.campsite.name}</h2>
-                            <hr />
+        return(
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map( comment => { 
+                
+                    return(
+                        <div key={comment.id}>
+                        <p>{comment.text}</p>
+                        <p>-- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                         </div>
-                    </div>
-                    <div className="row"> 
-                        <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                    );
+                    
+                    })}
+                <CommentForm/>
+            </div>
+        );
+
+    }else{
+        return <div> </div>;
+    }
+
+}
+
+
+function CampsiteInfo(props){
+
+        //if campsite is not null or undefined set to a row else a empty div
+    if(props.campsite){
+            return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
                     </div>
                 </div>
-            );
-        }
-            return <div />;
-        
-
-
+                <div className="row"> 
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+        );
     }
+        return <div />;
+    
+
+
+}
+
+class CommentForm extends React.Component{
+
+render(){
+
+    return(
+        <Button outline><i class="fa fa-pencil fa-lg" aria-hidden="true"></i>Submit Comment</Button>
+    );
+
+}
+
+
+}
 
 
 
