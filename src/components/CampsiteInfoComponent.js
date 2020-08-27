84fb6e3308bc5a1089cb0a24e979172f8a453382
Blaine Young <blaine.young3553@gmail.com>
2020-08-27 10:38:49 -0400

    fetch post comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -29,7 +29,7 @@ function RenderCampsite({campsite}){
 
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
 
     if(comments){
 
@@ -46,7 +46,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                     );
                     
                     })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
 
@@ -94,11 +94,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row"> 
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
-                    />
+                    /> 
                 </div>
             </div>
         );
@@ -123,8 +123,7 @@ toggleModal = () => {
 
 handleSubmit = (values) => {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
-    console.log(this.props)
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
 }
 
 render(){
